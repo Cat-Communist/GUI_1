b814ef23f6fa4f373e4705a97797973b5afcb93b
Cat-Communist <popovvp2502@gmail.com>
2026-03-10 13:18:22 +0800

    Made simple Form, implemented Logic class

---
+++ Bank_Deposit/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Bank_Deposit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            txtInitialDeposit = new TextBox();
+            txtExpectedIncrease = new TextBox();
+            txtExpectedDeposit = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            CalcMonthByIncr = new Button();
+            CalcMonthByDeposit = new Button();
+            SuspendLayout();
+            // 
+            // txtInitialDeposit
+            // 
+            txtInitialDeposit.Location = new Point(237, 13);
+            txtInitialDeposit.Name = "txtInitialDeposit";
+            txtInitialDeposit.Size = new Size(125, 27);
+            txtInitialDeposit.TabIndex = 0;
+            // 
+            // txtExpectedIncrease
+            // 
+            txtExpectedIncrease.Location = new Point(237, 46);
+            txtExpectedIncrease.Name = "txtExpectedIncrease";
+            txtExpectedIncrease.Size = new Size(125, 27);
+            txtExpectedIncrease.TabIndex = 1;
+            // 
+            // txtExpectedDeposit
+            // 
+            txtExpectedDeposit.Location = new Point(237, 79);
+            txtExpectedDeposit.Name = "txtExpectedDeposit";
+            txtExpectedDeposit.Size = new Size(125, 27);
+            txtExpectedDeposit.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(22, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(208, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Сумма вклада";
+            // 
+            // label2
+            // 
+            label2.Location = new Point(22, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(208, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Ожидаемый размер вклада";
+            // 
+            // label3
+            // 
+            label3.Location = new Point(22, 49);
+            label3.Name = "label3";
+            label3.Size = new Size(208, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Ожидаемое увеличение";
+            // 
+            // label4
+            // 
+            label4.Location = new Point(22, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(565, 107);
+            label4.TabIndex = 6;
+            label4.Text = resources.GetString("label4.Text");
+            // 
+            // CalcMonthByIncr
+            // 
+            CalcMonthByIncr.Location = new Point(368, 45);
+            CalcMonthByIncr.Name = "CalcMonthByIncr";
+            CalcMonthByIncr.Size = new Size(219, 29);
+            CalcMonthByIncr.TabIndex = 7;
+            CalcMonthByIncr.Text = "Рассчитать месяц";
+            CalcMonthByIncr.UseVisualStyleBackColor = true;
+            CalcMonthByIncr.Click += CalcMonthByIncr_Click;
+            // 
+            // CalcMonthByDeposit
+            // 
+            CalcMonthByDeposit.Location = new Point(368, 77);
+            CalcMonthByDeposit.Name = "CalcMonthByDeposit";
+            CalcMonthByDeposit.Size = new Size(219, 29);
+            CalcMonthByDeposit.TabIndex = 8;
+            CalcMonthByDeposit.Text = "Рассчитать кол-во месяцев";
+            CalcMonthByDeposit.UseVisualStyleBackColor = true;
+            CalcMonthByDeposit.Click += CalcMonthByDeposit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(610, 232);
+            Controls.Add(CalcMonthByDeposit);
+            Controls.Add(CalcMonthByIncr);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtExpectedDeposit);
+            Controls.Add(txtExpectedIncrease);
+            Controls.Add(txtInitialDeposit);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtInitialDeposit;
+        private TextBox txtExpectedIncrease;
+        private TextBox txtExpectedDeposit;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button CalcMonthByIncr;
+        private Button CalcMonthByDeposit;
+    }
+}
---
+++ Bank_Deposit/Form1.cs
@@ -0,0 +1,174 @@
+namespace Bank_Deposit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcMonthByIncr_Click(object sender, EventArgs e)
+        {
+            double initialDeposit, expectedIncrease;
+            try
+            {
+                initialDeposit = Logic.ReadFrom(txtInitialDeposit);
+                expectedIncrease = Logic.ReadFrom(txtExpectedIncrease);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message, "ќшибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "ќшибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var declinedMessage = Logic.DeclineBySum(expectedIncrease);
+
+            MessageBox.Show($"¬ этот мес€ц ежемес€чное увеличение превысит {declinedMessage}: " +
+                Logic.CalcIncrease(initialDeposit, expectedIncrease));
+        }
+
+        private void CalcMonthByDeposit_Click(object sender, EventArgs e)
+        {
+            double initialDeposit, expectedDeposit;
+            try
+            {
+                initialDeposit = Logic.ReadFrom(txtInitialDeposit);
+                expectedDeposit = Logic.ReadFrom(txtExpectedDeposit);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message, "ќшибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message, "ќшибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var declinedMessage = Logic.DeclineBySum(expectedDeposit);
+            MessageBox.Show($"„ерез это кол-во мес€цев сумма вклада превысит {declinedMessage}: " +
+                Logic.CalcDeposit(initialDeposit, expectedDeposit));
+        }
+    }
+
+    public class Logic
+    {
+        private static DateOnly initialDate = new DateOnly(2026, 3, 1);
+
+        public static double ReadFrom(TextBox inputField)
+        {
+            var result = 0d;
+            try
+            {
+                // берЄм только 2 цифры после зап€той
+                var input = double.Parse(inputField.Text);
+                result = Math.Truncate(input * 100) / 100;
+                if (result < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(inputField), "—умма не может быть меньше нул€");
+                }
+            }
+            catch (FormatException e)
+            {
+                throw;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public static string CalcIncrease(double initialDeposit, double expectedIncrease)
+        {
+            var month = 0;
+            while (true)
+            {
+                var currIncrease = Math.Round(initialDeposit * 0.02, 2);
+                initialDeposit += currIncrease;
+                if (currIncrease > expectedIncrease)
+                    return initialDate.AddMonths(month).ToString("MMMM");
+                month++;
+            }
+        }
+
+        public static int CalcDeposit(double initialDeposit, double expectedDeposit)
+        {
+            if (initialDeposit > expectedDeposit)
+                return 0;
+
+            var currDeposit = initialDeposit;
+            var month = 1;
+            while (true)
+            {
+                currDeposit = Math.Round(currDeposit * 1.02, 2);
+                if (currDeposit > expectedDeposit)
+                    return month;
+                month++;
+            }
+        }
+
+        public static string DeclineBySum(double inputSum)
+        {
+            int rubles = (int)inputSum;
+            int cents = (int)(inputSum * 100 % 100);
+
+            var rubleLine = "";
+            var centLine = "";
+            if (rubles >= 10 && rubles <= 14 || rubles % 10 == 0)
+            {
+                rubleLine = " рублей";
+            }
+            else if (rubles % 10 == 1)
+            {
+                rubleLine = " рубль";
+            }
+            else if (rubles % 10 >= 2 && rubles % 10 <= 4)
+            {
+                rubleLine = " рубл€";
+            }
+            else if (rubles % 10 >= 5 && rubles % 10 <= 9)
+            {
+                rubleLine = " рублей";
+            }
+
+            if (cents >= 10 && cents <= 14 || cents % 10 == 0)
+            {
+                centLine = " копеек";
+            }
+            else if (cents % 10 == 1)
+            {
+                centLine = " копейка";
+            }
+            else if (cents % 10 >= 2 && cents % 10 <= 4)
+            {
+                centLine = " копейки";
+            }
+            else if (cents % 10 >= 5 && cents % 10 <= 9)
+            {
+                centLine = " копеек";
+            }
+
+            var outMsg = "";
+            if (rubles != 0 && cents != 0)
+                outMsg = rubles + rubleLine + " " + cents + centLine;
+            else if (cents != 0)
+                outMsg = cents + centLine;
+            else
+                outMsg = rubles + rubleLine + " ровно";
+
+            return outMsg;
+        }
+    }
+}
